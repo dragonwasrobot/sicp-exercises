87ddff7c5af465359f01d5a2ce240360e51d2d38
Peter Urbak <peter@dragonwasrobot.com>
2013-01-11 23:48:57 +0100

    1.1.8-1.2

--- 1-building-abstractions-with-procedures.scm
+++ 1-building-abstractions-with-procedures.scm
@@ -339,6 +339,63 @@ circumference
 
 ;; 1.1.8 Procedures as Black-Box Abstractions
 
+;; procedural abstraction
+
+(define (square x) (* x x))
+(define (square x) (exp (double (log x))))
+(define (double x) (+ x x))
+
+;; Local names
+
+(define (square x) (* x x))
+(define (square y) (* y y))
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x))
+		 0.001))
+
+;; bound variable, binds, free, scope, capturing
+
+;; Internal definitions and block structure
+
+(define (sqrt x)
+	(sqrt-iter 1.0 x))
+(define (sqrt-iter guess x)
+	(if (good-enough? guess x)
+			guess
+			(sqrt-iter (improve guess x))))
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.001))
+(define (improve guess x)
+	(average guess (/ x guess)))
+
+(define (sqrt x)
+	(define (good-enough? guess x)
+		(< (abs (- (square guess) x)) 0.001))
+	(define (improve guess x)
+		(average guess (/ x guess)))
+	(define (sqrt-iter guess x)
+		(if (good-enough? guess x)
+				guess
+				(sqrt-iter (improve guess x) x)))
+	(sqrt-iter 1.0 x))
+
+;; block structure, lexical scoping
+
+(define (sqrt x)
+	(define (good-enough? guess)
+		(< (abs (- (square guess) x)) 0.001))
+	(define (improve guess)
+		(average guess (/ x guess)))
+	(define (sqrt-iter guess)
+		(if (good-enough? guess)
+				guess
+				(sqrt-iter (improve guess))))
+	(sqrt-iter 1.0))
+
+;; 1.2 Procedures and the Processes They Generate
+
+
 
 
 ;; end-of-1-building-abstractions-with-procedures.scm
