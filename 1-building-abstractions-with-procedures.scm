c7666b5a34579b66536abb91e389852a37b51ced
Peter Urbak <peter@dragonwasrobot.com>
2013-03-22 02:10:20 +0100

    proofread up to exercise 1.32

--- 1-building-abstractions-with-procedures.scm
+++ 1-building-abstractions-with-procedures.scm
@@ -941,23 +941,55 @@ circumference ;; 62.8318
 (integral cube 0 1 0.01) ;; 0.24998750000000042
 (integral cube 0 1 0.001) ;; 0.249999875000001
 
-;; Exercise 1.29
+;; ##### Exercise 1.29
+;; Simpson’s Rule is a more accurate method of numerical integration than the
+;; method illustrated above. Using Simpson's Rule, the integral of a function
+;; *f* between *a* and *b* is approximated as h/3(y_0 + 4y_1 + 2y_2 + 4y_3 +
+;; 2y_4 + ... + 2y_{n-2} + 4y_{n-1} + y_n), where *h = (b-a)/n*, for some even
+;; integer *n*, and *y_k = f (a + kh)*.  (Increasing *n* increases the accuracy
+;; of the approximation.) Define a procedure that takes as arguments *f*, *a*,
+;; *b*, and *n* and returns the value of the integral, computed using Simpson’s
+;; Rule. Use your procedure to integrate `cube` between 0 and 1 (with *n = 100*
+;; and *n = 1000*), and compare the results to those of the `integral` procedure
+;; shown above.
 
 ;; TODO
 
-;; Exercise 1.30
+;; ##### Exercise 1.30
+;; The `sum` procedure above generates a linear recursion. The procedure can be
+;; rewritten so that the sum is performed iteratively. Show how to do this by
+;; filling in the missing expressions in the following definition:
+
+;;    (define (sum term a next b)
+;;      (define (iter a result)
+;;        (if <??>
+;;            <??>
+;;            (iter <??> <??>)))
+;;      (iter <??> <??>))
 
 ;; TODO
 
-;; Exercise 1.31
+;; ##### Exercise 1.31
+;; a. The `sum` procedure is only the simplest of a vast number of similar
+;; abstractions that can be captured as higher-order procedures. Write an
+;; analogous procedure called `product` that returns the product of the values
+;; of a function at points over a given range. Show how to define `factorial` in
+;; terms of `product`. Also use `product` to compute approximations to pi using
+;; the formula
+;; pi/4 = (2*4*4*6*6*8...)/(3*3*5*5*7*7...)
+
+;; b. If your `product` procedure generates a recursive process, write one that
+;; generates an iterative process. If it generates an iterative process, write
+;; one that generates a recursive process.
 
 ;; TODO
 
-;; Exercise 1.32
+;; ##### Exercise 1.32
+
 
 ;; TODO
 
-;; Exercise 1.33
+;; ##### Exercise 1.33
 
 ;; TODO
 
