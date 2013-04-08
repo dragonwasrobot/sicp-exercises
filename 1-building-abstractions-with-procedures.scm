d6f9d73d98b0143e3cd467fa4aa667e2075232b6
Peter Urbak <peter@dragonwasrobot.com>
2013-04-08 23:09:27 +0200

    Finished markup of chapter 1 exercises.

--- 1-building-abstractions-with-procedures.scm
+++ 1-building-abstractions-with-procedures.scm
@@ -1382,15 +1382,27 @@ euler-e-frac
 
 ;; ##### Exercise 1.40
 
+;; Define a procedure `cubic` that can be used together with the
+;; `newtons-method` procedure in expressions of the form
+
+;;    (newtons-method (cubic a b c) 1)
+
+;; to approximate zeroes of the cubic x^3 + ax^2 + bx + c
+
 (define (cubic a b c)
   (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
 
-;; Approximate zeroes of the cubic x^3 + ax^2 + bx + c
-;; (newtons-method (cubic a b c) 1)
 (newtons-method (cubic 3 4 5) 1)
 ;; -2.2134116627621956
 
-;; Exercise 1.41
+;; ##### Exercise 1.41
+
+;; Define a procedure `double` that takes a procedure of one argument as
+;; argument and returns a procedure that applies the original procedure
+;; twice. For example, if `inc` is a procedure that adds 1 to its argument, then
+;; `(double inc)` should be a procedure that adds 2. What value is returned by
+
+;;    (((double (double double)) inc) 5)
 
 (define (double f)
   (lambda (x) (f (f x))))
@@ -1401,7 +1413,15 @@ euler-e-frac
 (((double (double double)) inc) 5)
 ;; 21
 
-;; Exercise 1.42
+;; ##### Exercise 1.42
+
+;; Let *f* and *g* be two one-argument functions. The *composition f* after *g*
+;; is defined to be the function *x -> f(g(x))*. Define a procedure `compose`
+;; that implements composition. For example, if `inc` is a procedure that adds 1
+;; to its argument,
+
+;;    ((compose square inc) 6)
+;;    49
 
 (define (compose f g)
   (lambda (x) (f (g x))))
@@ -1409,7 +1429,21 @@ euler-e-frac
 ((compose square inc) 6)
 ;; 49
 
-;; Exercise 1.43
+;; ##### Exercise 1.43
+
+;; If *f* is a numerical function and *n* is a positive integer, then we can
+;; form the *n^th* repeated application of *f*, which is defined to be the
+;; function whose value at *x* is *f(f(...(f(x))...)).* For example, if *f* is
+;; the function *x -> x + 1*, then the *n^th* repeated application of *f* is the
+;; function *x -> x + n*. If *f* is the operation of squaring a number then the
+;; *n^th* repeated application of *f* is the function that raises its argument
+;; to the *2^n-th* power. Write a procedure that takes as inputs a procedure
+;; that computes *f* and a positive integer *n* and returns the procedure that
+;; computes the *n^th* repeated application of *f*. Your procedure should be
+;; able to be used as follows:
+
+;;    ((repeated square 2) 5)
+;;    625
 
 (define (repeated f n)
   (if (= n 0)
@@ -1419,7 +1453,18 @@ euler-e-frac
 ((repeated square 2) 5)
 ;; 625
 
-;; Exercise 1.44
+;; ##### Exercise 1.44
+
+;; The idea of *smoothing* a function is an important concept in signal
+;; processing. If *f* is a function and *dx* is some small number, then the
+;; smoothed version of *f* is the function whose value at a point *x* is the
+;; average of *f(x - dx)*, *f(x)*, and *f(x + dx)*. Write a procedure `smooth`
+;; that takes as input a procedure that computes *f* and returns a procedure
+;; that computes the smoothed *f*.  It is sometimes valuable to repeatedly
+;; smooth a function (that is, smooth the smoothed function, and so on) to
+;; obtain the *n-fold smoothed function*. Show how to generate the *n*-fold
+;; smoothed function of any given function using `smooth` and `repeated` from
+;; Exercise 1.43.
 
 (define (smooth f)
   (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)))
@@ -1433,7 +1478,21 @@ euler-e-frac
 ((smooth-n-fold cube 4) 2)
 ;; 8.0000000016
 
-;; Exercise 1.45
+;; ##### Exercise 1.45
+
+;; We saw in Section 1.3.3 that attempting to compute square roots by naively
+;; ﬁnding a ﬁxed point of *y -> x/y* does not converge, and that this can be
+;; ﬁxed by average damping. The same method works for ﬁnding cube roots as ﬁxed
+;; points of the averagedamped *y -> x/y^2*. Unfortunately, the process does not
+;; work for fourth roots - a single average damp is not enough to make a
+;; ﬁxedpoint search for *y -> x/y^3* converge. On the other hand, if we average
+;; damp twice (i.e., use the average damp of the average damp of *y -> x/y^3*)
+;; the ﬁxed-point search does converge. Do some experiments to determine how
+;; many average damps are required to compute *n^th* roots as a ﬁxed-point
+;; search based upon repeated average damping of *y -> x/y^{n-1}* . Use this to
+;; implement a simple procedure for computing *n^th* roots using `fixed-point`,
+;; `average-damp`, and the `repeated` procedure of Exercise 1.43. Assume that
+;; any arithmetic operations you need are available as primitives.
 
 (define (cube-roots x)
   (fixed-point (average-damp (lambda (y) (/ x (square y)))) 1.0))
@@ -1448,7 +1507,19 @@ euler-e-frac
 (n-root 64 4)
 ;; 2.82842712474619
 
-;; Exercise 1.46
+;; ##### Exercise 1.46
+
+;; Several of the numerical methods described in this chapter are instances of
+;; an extremely general computational strategy known as *iterative
+;; improvement*. Iterative improvement says that, to compute something, we start
+;; with an initial guess for the answer, test if the guess is good enough, and
+;; otherwise improve the guess and continue the process using the improved guess
+;; as the new guess. Write a procedure `iterative-improve` that takes two
+;; procedures as arguments: a method for telling whether a guess is good enough
+;; and a method for improving a guess. `Iterative-improve` should return as its
+;; value a procedure that takes a guess as argument and keeps improving the
+;; guess until it is good enough. Rewrite the `sqrt`procedure of Section 1.1.7 and
+;; the `fixed-point` procedure of Section 1.3.3 in terms of `iterative-improve`.
 
 (define (iterative-improvement good-enough? improve-guess)
   (define (lambda-rec guess)
@@ -1469,7 +1540,5 @@ euler-e-frac
   ((iterative-improvement (lambda (guess next) (< (abs (- guess next)) 0.00001))
                           f)
    first-guess))
-
-
 
 ;; end-of-1-building-abstractions-with-procedures.scm
--- README.md
+++ README.md
@@ -6,11 +6,14 @@ This is a collection of solutions to the exercises found in 'Structure and
 Interpretation of Computer Programs' by Harold Abelson, Gerald Jay Sussman and
 Julie Sussman.
 
+Exercise descriptions from the book have been included in the source code for
+readability of the code solutions.
+
 ## About
 
 - **Author:** Peter Urbak
 - **Created:** 2013-01-11
-- **Version:** 2013-03-21
+- **Version:** 2013-04-08
 - **URL:** https://github.com/dragonwasrobot/sicp-exercises
 
 ## To-do
