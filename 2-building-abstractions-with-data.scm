7b7adb170206cc4b0de30ce3a193ae2de0ed125f
Peter Urbak <peter@dragonwasrobot.com>
2013-06-27 14:47:57 +0200

    Updated README.md

--- 2-building-abstractions-with-data.scm
+++ 2-building-abstractions-with-data.scm
@@ -1215,7 +1215,16 @@ x ;; '((1 2) (3 4))
 ;; `(horner-eval 2 (list 1 3 0 5 0 1))`
 
 ;; (+ (* (+ (* (+ (* (+ (* (+ (* 1 x) (* 0 x)) x) 5) x) 0) x) 3) x) 1)
-(horner-eval 2 (list 1 3 0 5 0 1)) ;; 79
+(horner-eval 3 (list 1 0 5 0 3 1))
+;; (horner-eval 2 (list 1 3 0 5 0 1)) ;; 79
+
+(horner-eval 0 (list 3 2 1)) ;; 3
+(horner-eval 1 (list 3 2 1)) ;; 6
+(horner-eval 2 (list 3 2 1)) ;; 11
+(horner-eval 3 (list 3 2 1)) ;; 18
+(horner-eval 4 (list 3 2 1)) ;; 27
+(horner-eval 5 (list 3 2 1)) ;; 38
+
 
 ;; ##### Exercise 2.35
 
@@ -1514,4 +1523,107 @@ x ;; '((1 2) (3 4))
 
 ;; ##### Exercise 2.42
 
+;; The "eight-queens puzzle" asks how to place eight queens on a chessboard so
+;; that no queen is in check from any other (i.e., no two queens are in the same
+;; row, column, or diagonal). One possible solution is shown in Figure 2.8. One
+;; way to solve the puzzle is to work across the board, placing a queen in each
+;; column. Once we have placed *k − 1* queens, we must place the *k^{th}* queen
+;; in a position where it does not check any of the queens already on the
+;; board. We can formulate this approach recursively: Assume that we have
+;; already generated the sequence of all possible ways to place *k − 1* queens
+;; in the first *k − 1* columns of the board. For each of these ways, generate
+;; an extended set of positions by placing a queen in each row of the *k^{th}*
+;; column. Now filter these, keeping only the positions for which the queen in
+;; the *k^{th}* column is safe with respect to the other queens. This produces
+;; the sequence of all ways to place *k* queens in the first *k* columns. By
+;; continuing this process, we will produce not only one solution, but all
+;; solutions to the puzzle.
+
+;; We implement this solution as a procedure `queens`, which returns a sequence
+;; of all solutions to the problem of placing *n* queens on an *n * n*
+;; chessboard. Queens has an internal procedure `queen-cols` that returns the
+;; sequence of all ways to place queens in the first *k* columns of the board.
+
+(define (queens board-size)
+  (define (queen-cols k)
+      (if (= k 0)
+          (list empty-board)
+          (filter
+           (lambda (positions) (safe? k positions))
+           (flatmap
+            (lambda (rest-of-queens)
+              (map (lambda (new-row)
+                     (adjoin-position new-row
+                                      k
+                                      rest-of-queens))
+                   (enumerate-interval 1 board-size)))
+            (queen-cols (- k 1))))))
+    (queen-cols board-size))
+
+;; In this procedure `rest-of-queens` is a way to place *k − 1* queens in the
+;; first *k − 1* columns, and `new-row` is a proposed row in which to place the
+;; queen for the *k^{th}* column. Complete the program by implementing the
+;; representation for sets of board positions, including the procedure
+;; `adjoin-position`, which adjoins a new row-column position to a set of
+;; positions, and `empty-board`, which represents an empty set of positions. You
+;; must also write the procedure `safe?`, which determines for a set of
+;; positions, whether the queen in the *k^{th}* column is safe with respect to
+;; the others. (Note that we need only check whether the new queen is safe - the
+;; other queens are already guaranteed safe with respect to each other).
+
+(define (make-position row column)
+  (cons row column))
+
+(define (position-row position)
+  (car position))
+
+(define (position-column position)
+  (cdr position))
+
+(define empty-board '())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (append rest-of-queens (list (make-position new-row k))))
+
+(define (safe? k positions) #t)
+
+(define (safe? k positions)
+  (print k)
+  (print positions)
+
+  (define (rows-safe? k positions)
+    (cond ((null? positions) #t)
+          ((= (position-row k) (position-row (car positions))) #f)
+          (else (rows-safe? k (cdr positions)))))
+
+  (define (diagonal-safe? k positions)
+    (cond ((null? positions) #t)
+          ((= (abs (- (position-row k)
+                      (position-row (car positions))))
+              (abs (- (position-column k)
+                      (position-column (car positions)))))
+           #f)
+          (else (diagonal-safe? k (cdr positions)))))
+
+  (and (rows-safe? k positions)
+       (diagonal-safe? k positions)))
+
 ;; TODO
+
+;; ##### Exercise 2.43
+
+;; Louis Reasoner is having a terrible time doing Exercise 2.42. His queens
+;; procedure seems to work, but it runs extremely slowly. (Louis never does
+;; manage to wait long enough for it to solve even the 6 x 6 case.) When Louis
+;; asks Eva Lu Ator for help, she points out that he has interchanged the order
+;; of the nested mappings in the flatmap, writing it as
+
+;;    (flatmap (lambda (new-row)
+;;      (map (lambda (rest-of-queens) (adjoin-position new-row k rest-of-queens))
+;;           (queen-cols (- k 1)))) (enumerate-interval 1 board-size))
+
+;; Explain why this interchange makes the program run slowly. Estimate how long
+;; it will take Louis' program to solve the eight-queens puzzle, assuming that
+;; the program in Exercise 2.42 solves the puzzle in time T.
+
+;; ### 2.2.4 Example: A Picture Language
