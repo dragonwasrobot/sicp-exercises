46bc48224ca69c2bdbde04c67f2a391991b45531
Peter Urbak <peter@dragonwasrobot.com>
2013-06-27 16:45:05 +0200

    -> Exercise 2.44

--- 2-building-abstractions-with-data.scm
+++ 2-building-abstractions-with-data.scm
@@ -1627,3 +1627,49 @@ x ;; '((1 2) (3 4))
 ;; the program in Exercise 2.42 solves the puzzle in time T.
 
 ;; ### 2.2.4 Example: A Picture Language
+
+;; #### The picture language
+
+(define wave2 (beside wave (flip-vert wave)))
+(define wave4 (below wave2 wave2))
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4 (flipped-pairs wave))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+;; ##### Exercise 2.44
+
+;; Define the procedure `up-split` used by `corner-split`. It is similar to
+;; `right-split`, except that it switches the roles of `below` and `beside`.
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+;; #### Higher-order operations
