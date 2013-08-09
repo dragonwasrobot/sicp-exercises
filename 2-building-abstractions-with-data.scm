3c0876aa52d85f77b46931afa3bcfb5f72d40a81
Peter Urbak <peter@dragonwasrobot.com>
2013-08-08 17:31:54 -0700

    -> 2.3.4 Example: Huffman Coding Trees

--- 2-building-abstractions-with-data.scm
+++ 2-building-abstractions-with-data.scm
@@ -2808,3 +2808,27 @@ x ;; '((1 2) (3 4))
 ;;  8 ->  / , 10
 
 ;; #### Sets and information retrieval
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records)
+         #f)
+        ((= given-key (key (car set-of-records)))
+         (car set-of-records))
+        (else (lookup given-key (cdr set-of-records)))))
+
+;; ##### Exercise 2.66
+
+;; Implement the `lookup` procedure for the case where the set of records is
+;; structured as a binary tree, ordered by the numerical values of the keys.
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records)
+         #f)
+        ((= given-key (key (entry set-of-records)))
+         (entry set-of-records))
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup given-key (right-branch set-of-records)))))
+
+;; ### 2.3.4 Example: Huffman Encoding Trees
